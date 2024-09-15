d3bcb7f7fee26a2b74aa926f7e648746cc34e434
OneNull <shavrin_ns@mail.ru>
2024-09-15 14:57:04 +0300

    Final version

--- src/DrawBackground.java
+++ src/DrawBackground.java
@@ -5,6 +5,7 @@ public class DrawBackground {
     public DrawBackground(Graphics2D g2d, int x, int y, int width, int height) {
         g2d.setColor(Color.CYAN);
         g2d.fillRect(x, y, width, height);
+
         g2d.setColor(Color.GREEN);
         g2d.fillRect(x, y + (height / 10 * 9), width, height);
     }
